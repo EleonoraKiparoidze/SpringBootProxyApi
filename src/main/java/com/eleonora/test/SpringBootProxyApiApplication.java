2566da871b24a82eb08b9c5ce38e45c6a68b4155
EleonoraNora <kiparoidzie@gmail.com>
2018-11-14 18:53:14 +0200

    Initial commit

---
+++ src/main/java/com/eleonora/test/SpringBootProxyApiApplication.java
@@ -0,0 +1,12 @@
+package com.eleonora.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProxyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProxyApiApplication.class, args);
+	}
+}
